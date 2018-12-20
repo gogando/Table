61621dcc8f05060e91bca64350d3cab6934eb66e
Adam Gandossi <adam.gandossi@thalesgroup.com.au>
2018-12-20 09:40:19 +0800

    Initial contact layer

--- renderer.js
+++ renderer.js
@@ -4,7 +4,7 @@
 
 const interact = require('interactjs')
 const L = require('leaflet')
-const tracks = require('./tracks')
+const fs = require('fs')
 
 const map = L.map('map', {
     center:[-31.9505, 115.8605],
@@ -14,7 +14,56 @@ const map = L.map('map', {
     dragging: false
 })
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+//L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+//L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(map)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png').addTo(map)
+
+// Contacts
+L.TrackMarker = L.Marker.extend({
+
+    options: {
+        rotation: 0
+    },
+
+    _setPos: function(pos) {
+        L.Marker.prototype._setPos.call(this, pos);
+        if(L.DomUtil.TRANSFORM) {
+            this._icon.style[L.DomUtil.TRANSFORM] += ' rotate(' + this.options.rotation + 'deg)';
+        }
+    }
+
+});
+
+fs.readFile('aishub-oz.json', 'utf8', (err, fileContents) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  try {
+    const tracks = JSON.parse(fileContents)
+    console.log(tracks)
+    for (var i = tracks.length - 1; i >= 0; i--) {
+        var track = tracks[i];
+       
+        var getColour = function(type) {
+            var index = Number(String(type).charAt(0));
+            return ['yellow', 'yellow', 'yellow', 'green', 'cyan', 'yellow', 'blue', 'green', 'red', 'purple'][index];
+        };
+
+        var marker = new L.TrackMarker([track.LATITUDE, track.LONGITUDE]);
+        if(track.SOG > 0) {
+            marker.options.icon = L.divIcon({ className: 'marker-icon ship-medium ' + getColour(track.TYPE)});
+            marker.options.rotation = track.COG;
+        } else {
+            marker.options.icon = L.divIcon({ className: 'marker-icon ship-medium ' + getColour(track.TYPE) + ' stationary'});
+        }
+
+           marker.addTo(map);
+    }
+  } catch(err) {
+    console.error(err)
+  }
+})
 
 // TODO: Make it possible to rotate items
 
